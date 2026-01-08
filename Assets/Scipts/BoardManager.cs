9cfc8eae4036dc5837178ae7992cd28ae474ccde
Nathen Afshari <nathen.afshari@gmail.com>
2026-01-07 19:41:22 -0600

    Added class CellData for each tile.
    Sets wall tiles to be impassable and all others to be passable.

--- Assets/Scipts/BoardManager.cs
+++ Assets/Scipts/BoardManager.cs
@@ -3,31 +3,41 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+
     private Tilemap m_Tilemap;
+    private CellData[,] m_BoardData;
 
     public int width;
     public int height;
     public Tile[] groundTiles;
-   public Tile[] wallTiles;
+    public Tile[] wallTiles;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_BoardData = new CellData[width, height];
 
         for (int y = 0; y < height; ++y)
         {
            for(int x = 0; x < width; ++x)
            {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if(x == 0 || y == 0 || x == width - 1 || y == height - 1)
                 {
                     tile = wallTiles[Random.Range(0, wallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = groundTiles[Random.Range(0, groundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
                 }
               
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
